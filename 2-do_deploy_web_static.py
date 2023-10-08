3696d60b6ad98ec408ea7421edec4c4ef0d3daf6
Shewanji <marrionwanjiru26@gmail.com>
2023-10-08 11:08:56 -0700

    test deploy archive

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,66 +3,52 @@
 
 from fabric.api import env, put, run
 from datetime import datetime
-import os
+from os import path
 
 env.hosts = ['18.209.178.16', '54.173.85.21']
 env.user = 'ubuntu'
-
-
-def do_pack():
-    """
-        return the archive path if archive has generated correctly
-    """
-
-    local("mkdir -p versions")
-    date = datetime.now().strftime("%Y%m%d%H%M%S")
-    archived_f_path = "versions/web_static_{}.tgz".format(date)
-    t_gzip_archive = local("tar -cvzf {} web_static".format(archived_f_path))
-
-    if t_gzip_archive.succeeded:
-        return archived_f_path
-    else:
-        return None
+env.key_filename = '~/.ssh/id_rsa'
 
 
 def do_deploy(archive_path):
-    """Distributes an archive to web servers.
+    """Distribute an archive to the web servers and deploy it.
 
     Args:
-        archive_path (str): Path to the archive to be deployed.
+        archive_path (str): The path to the archive to deploy.
 
     Returns:
-        bool: True if all operations are successful, False otherwise.
+        bool: True if deployment is successful, False otherwise.
     """
-    if not os.path.exists(archive_path):
+    if not (path.exists(archive_path)):
         return False
 
     try:
-        tmp_archive = "/tmp/{}".format(archive_path.split("/")[1])
-        releases = "/data/web_static/releases/"
-        archive_name = archive_path.split("/")[1].split(".")[0]
-        current = "/data/web_static/current"
-        full_path = "{}{}/".format(releases, archive_name)
+        # Upload the archive to /tmp/ directory on the web servers
+        put(archive_path, '/tmp/')
 
-        put(archive_path, tmp_archive)
+        # Extract archive to /data/web_static/releases/<archive_filename_without_extension>/
+        archive_filename = os.path(archive_path)
+        folder_name = archive_filename.replace('.tgz', '')
 
-        run("cd /home/{}".format(env.user))
+        run('mkdir -p /data/web_static/releases/{}/'.format(folder_name))
+        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'
+            .format(archive_filename, folder_name))
 
-        run("sudo mkdir -p {}".format(full_path))
+        # Remove the uploaded archive
+        run('rm /tmp/{}'.format(archive_filename))
 
-        run("sudo tar -xzf {} -C {}".format(tmp_archive,
-                                            full_path))
+        # Move files to final location
+        run('mv /data/web_static/releases/{}/web_static/* '
+            '/data/web_static/releases/{}/'.format(folder_name, folder_name))
 
-        run("sudo rm -rf {}".format(tmp_archive))
+        # Remove the old symbolic link
+        run('rm -rf /data/web_static/current')
 
-        run("sudo mv {}web_static/* {}".format(full_path, full_path))
-
-        run("sudo rm -rf /data/web_static/current")
-
-        run("sudo ln -sf {} {}".format(full_path, current))
-
-        print("New version deployed!")
+        # Create a new symbolic link
+        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'
+            .format(folder_name))
 
         return True
+
     except Exception as e:
         return False
