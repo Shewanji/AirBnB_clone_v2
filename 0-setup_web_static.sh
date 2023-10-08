c9c8935e2dbb7bbb7c9cdafaa8c5aa82435ce4ff
Shewanji <marrionwanjiru26@gmail.com>
2023-10-08 09:42:02 -0700

    fix Bash script that sets up your web servers for the deployment of web_static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,17 +1,14 @@
 #!/usr/bin/env bash
-#Bash script that sets up your web servers for the deployment of web_statici
+# Bash script that sets up web servers for the deployment of web_static
+sudo apt-get update
+sudo apt-get -y install nginx
 
-# Install Nginx if not already installed
-if ! command -v nginx &> /dev/null; then
-    sudo apt-get -y update
-    sudo apt-get -y install nginx
-fi
-
-# Create necessary directories
-sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
 sudo mkdir -p /data/web_static/shared/
-
-# Create a fake HTML file
+sudo mkdir -p /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
 sudo echo "<html>
   <head>
   </head>
@@ -20,23 +17,10 @@ sudo echo "<html>
   </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
 
-# Create or recreate the symbolic link
-if [ -L /data/web_static/current ]; then
-    rm /data/web_static/current
-fi
+sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-# Give ownership to the ubuntu user and group
 sudo chown -R ubuntu:ubuntu /data/
 
-# Update Nginx configuration
-config_file="/etc/nginx/sites-available/default"
-sudo sed -i " /location \/hbnb_static {/ a\\
-        alias /data/web_static/current/;
-" $config_file
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 
-# Restart Nginx
 sudo service nginx restart
-
-exit 0
