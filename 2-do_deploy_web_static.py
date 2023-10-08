34691d8ab8713ee72fdd11e04fcfb21c0df339ae
Shewanji <marrionwanjiru26@gmail.com>
2023-10-08 12:49:09 -0700

    Fabric script (based on the file 1-pack_web_static.py) that distributes an archive to your web servers, using the function do_deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,6 +8,7 @@ from os import path
 env.hosts = ['18.209.178.16', '54.173.85.21']
 env.user = 'ubuntu'
 env.key_filename = '~/.ssh/id_rsa'
+env.warn_only = True
 
 
 def do_deploy(archive_path):
@@ -20,7 +21,7 @@ def do_deploy(archive_path):
     bool: True if deployment is successful, False otherwise.
     """
     try:
-        if not (path.exists(archive_path)):
+        if not path.exists(archive_path):
             return False
 
         put(archive_path, '/tmp/')
@@ -29,24 +30,34 @@ def do_deploy(archive_path):
         run('sudo mkdir -p /data/web_static/\
 releases/web_static_{}/'.format(timestamp))
 
+        run('sudo mkdir -p /data/web_static/\
+releases/web_static_{}/'.format(timestamp))
+
+        # uncompress archive and delete .tgz
         run('sudo tar -xzf /tmp/web_static_{}.tgz -C \
 /data/web_static/releases/web_static_{}/'
             .format(timestamp, timestamp))
 
+        # remove archive
         run('sudo rm /tmp/web_static_{}.tgz'.format(timestamp))
 
+        # move contents into host web_static
         run('sudo mv /data/web_static/releases/web_static_{}/web_static/* \
 /data/web_static/releases/web_static_{}/'.format(timestamp, timestamp))
 
+        # remove extraneous web_static dir
         run('sudo rm -rf /data/web_static/releases/\
 web_static_{}/web_static'
             .format(timestamp))
 
+        # delete pre-existing sym link
         run('sudo rm -rf /data/web_static/current')
 
+        # re-establish symbolic link
         run('sudo ln -s /data/web_static/releases/\
 web_static_{}/ /data/web_static/current'.format(timestamp))
-        except Exception as e:
-            return False
 
-        return True
+    except Exception as e:
+        return False
+
+    return True
