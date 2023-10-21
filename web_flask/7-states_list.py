66e2b4fe65201318c330e0b3b453eebc1805d5ec
Shewanji <marrionwanjiru26@gmail.com>
2023-10-21 13:50:00 -0700

    List of states

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+A Flask web application that displays a list of states.
+"""
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """
+    Display a list of states retrieved from the storage engine.
+    """
+    states = storage.all(State).values()
+    sorted_states = sorted(states, key=lambda state: state.name)
+    return render_template('7-states_list.html', states=sorted_states)
+
+
+@app.teardown_appcontext
+def close_storage(exception):
+    """
+    Close the SQLAlchemy session after each request.
+    """
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
