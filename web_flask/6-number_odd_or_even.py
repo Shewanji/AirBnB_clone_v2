aca7849a38d3c330b143fbc99b5c486d919c8ed4
Shewanji <marrionwanjiru26@gmail.com>
2023-10-21 06:14:00 -0700

    Odd or even

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python3
+"""
+A Flask web application with multiple routes.
+"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Display "Hello HBNB!" on the root route."""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Display "HBNB" on the /hbnb route."""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """
+    Display "C " followed by the value of the text variable
+    (replace underscores with spaces).
+
+    :param text: The text to be displayed, with underscores replaced by spaces.
+    :type text: str
+    """
+    text_with_spaces = text.replace("_", " ")
+    return f'C {text_with_spaces}'
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text="is cool"):
+    """
+    Display "Python " followed by the value of the text variable
+    (replace underscores with spaces).
+
+    :param text: The text to be displayed, with underscores replaced by spaces.
+    :type text: str
+    """
+    text_with_spaces = text.replace("_", " ")
+    return f'Python {text_with_spaces}'
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """
+    Display "n is a number" only if n is an integer.
+
+    :param n: The number to be displayed.
+    :type n: int
+    """
+    return f'{n} is a number'
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    """
+    Display an HTML page with a title and an H1 tag showing "Number: n"
+    only if n is an integer.
+
+    :param n: The number to be displayed.
+    :type n: int
+    """
+    return render_template('5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>')
+def number_odd_or_even(n):
+    """Render template based on conditional
+    """
+    return render_template('6-number_odd_or_even.html', n=n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
